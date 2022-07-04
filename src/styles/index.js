153faf03a749e37e9b8d1430f5586d7a0aa0ec67
Pedro Mesquita <pedro.mesquita.hc@gmail.com>
2022-07-04 18:03:13 -0300

    feat: :sparkles: Adicionando tags css ao componente de estilo

--- src/styles/index.js
+++ src/styles/index.js
@@ -1,1 +1,8 @@
 import styled from "styled-components";
+
+export const PersonalInfo = styled.section`
+  width: 95%;
+  height: 100vh;
+  background: #1C1C1C;
+  clip-path: polygon(0% 0%, 50% 0, 65% 100%, 0% 100%);
+`;
