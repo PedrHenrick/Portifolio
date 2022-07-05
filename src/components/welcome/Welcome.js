7c7ae4210ca00e3af35210e543e6b74612cbd2a7
Pedro Mesquita <pedro.mesquita.hc@gmail.com>
2022-07-04 23:08:51 -0300

    feat: :sparkles: Adicionando seções e efeito typewriter

--- src/components/welcome/Welcome.js
+++ src/components/welcome/Welcome.js
@@ -1,12 +1,61 @@
-import React from "react";
-import { PersonalInfo } from '../../styles'; 
+import React, { useState, useEffect } from "react";
+import { PersonalInfo, SectionWelcome } from '../../styles'; 
+import BackgroundAnimate from "../BackgroundAnimate";
+
+const arrayWithName = [
+  'M',
+  'Me',
+  'Mes',
+  'Mesq',
+  'Mesqu',
+  'Mesqui',
+  'Mesquit',
+  'Mesquita',
+  'Mesquit',
+  'Mesqui',
+  'Mesqu',
+  'Mesq',
+  'Mes',
+  'Me',
+  'M',
+  '',
+  'H',
+  'He',
+  'Hen',
+  'Henr',
+  'Henri',
+  'Henric',
+  'Henrick',
+  'Henrick!',
+];
 
 const Welcome = () => {
+  const [lastName, setLastName] = useState('');
+  const [modify, setModify] = useState(false);
+
+  useEffect(() => {
+    const TypeWriter = () => {
+      arrayWithName.forEach((name, i) => {
+        setTimeout(() => {
+          setLastName(name);
+        }, 200 * i);
+      });
+    }
+    TypeWriter()
+  }, [modify]);
+
+
   return (
     <section>
-      <PersonalInfo>
-        <h1>Lalalala</h1>
-      </PersonalInfo>
+      <SectionWelcome>
+        <BackgroundAnimate />
+        <PersonalInfo>
+          <h3>Bem Vindo! Me chamo...</h3>
+          <h2>Pedro { lastName }</h2>
+          <h3>Estudante de desenvolvimento web na trybe!</h3>
+          <button>Download cv</button>
+        </PersonalInfo>
+      </SectionWelcome>
     </section>
   );
 };
