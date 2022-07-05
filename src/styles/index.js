9c9ae8a5f9d8bd7c7ae5f9aef7c3576ca110090c
Pedro Mesquita <pedro.mesquita.hc@gmail.com>
2022-07-04 23:11:06 -0300

    feat: :sparkles: Adicionando camadas de estilo

--- src/styles/index.js
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-export const PersonalInfo = styled.section`
-  width: 95%;
-  height: 100vh;
-  background: #1C1C1C;
-  clip-path: polygon(0% 0%, 50% 0, 65% 100%, 0% 100%);
-`;
---
+++ style/style.js
@@ -0,0 +1,77 @@
+import styled, { keyframes } from "styled-components";
+
+export const PersonalInfo = styled.section`
+  position: absolute;
+  width: 100%;
+  height: 100vh;
+  background: #1C1C1C;
+  display: flex;
+  color: white;
+  flex-direction: column;
+  align-items: flex-start;
+  clip-path: polygon(0% 0%, 50% 0, 65% 100%, 0% 100%);
+`;
+
+export const Background = styled.body`
+  position: relative;
+  width: 100%;
+  height: 100vh;
+  background-color: #111;
+  display: flex;
+  flex-direction: column;
+  overflow: hidden;
+`;
+
+export const Row = styled.div`
+  position: relative;
+  width: 100%;
+  display: flex;
+  padding: 10px 0;
+  white-space: nowrap;
+  font-size: 60px;
+  transform: rotate(-30deg);
+`;
+
+const animate1 = keyframes`
+  from {
+    transform: translateX(100%)
+  }
+
+  to {
+    transform: translateX(-100%)
+  }
+`;
+
+const animate2 = keyframes`
+  from {
+    transform: translateX(0%)
+  }
+
+  to {
+    transform: translateX(-200%)
+  }
+`;
+
+export const ConjuntIcons = styled.div`
+  animation: ${animate1} 80s linear infinite;
+  animation-delay: -80s;
+
+  &&:nth-child(2) {
+    animation: ${animate2} 80s linear infinite;
+    animation-delay: -40s;
+  }
+`;
+
+export const Icons = styled.ol`
+  display: inline;
+  color: rgba(0, 0, 0, 0.5);
+  transition: 1s;
+  padding: 0 6px;
+  user-select: none;
+  cursor:default;
+  &&:hover {
+    transition: 0s;
+    color: #0f0;
+    box-shadow: 0px 0px 120px #0f0;
+  }
+`;
