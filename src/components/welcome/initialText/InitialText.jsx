4aced79825994552410a96acd48dd191791084bb
Pedro Mesquita <pedro.mesquita.hc@gmail.com>
2022-07-07 21:13:27 -0300

    perf: :zap: Refatorando lógica adicionando lib instalada anteriormente

--- src/components/welcome/initialText/InitialText.jsx
+++ src/components/welcome/initialText/InitialText.jsx
@@ -1,57 +1,54 @@
-import React, { useEffect, useState } from 'react';
-
-const arrayWithName = [
-  'M',
-  'Me',
-  'Mes',
-  'Mesq',
-  'Mesqu',
-  'Mesqui',
-  'Mesquit',
-  'Mesquita',
-  'Mesquit',
-  'Mesqui',
-  'Mesqu',
-  'Mesq',
-  'Mes',
-  'Me',
-  'M',
-  '',
-  'H',
-  'He',
-  'Hen',
-  'Henr',
-  'Henri',
-  'Henric',
-  'Henrick',
-  'Henrick!',
-];
+import React from 'react';
+import TypeWriterEffect from 'react-typewriter-effect';
+import { BiCloudDownload } from 'react-icons/bi';
+import { BsLinkedin } from 'react-icons/bs';
+import { AiFillGithub } from 'react-icons/ai';
+import { FiTwitter } from 'react-icons/fi';
+import {
+  PersonalName,
+  SectionMain,
+  WelcomeText,
+  DescriptionText,
+  ButtonDownload,
+  ButtonBar,
+  ButtonLinkedin,
+  RedesNav,
+  Tecnology,
+} from './InitialText';
 
 export default function InitialText() {
-  const [lastName, setLastName] = useState('');
-  const [modify] = useState(false);
-
-  useEffect(() => {
-    const TypeWriter = () => {
-      arrayWithName.forEach((name, i) => {
-        setTimeout(() => {
-          setLastName(name);
-        }, 200 * i);
-      });
-    };
-    TypeWriter();
-  }, [modify]);
-
   return (
-    <section>
-      <h3>Bem Vindo! Me chamo...</h3>
-      <h2>
-        Pedro
-        { ' ' }
-        { lastName }
-      </h2>
-      <h3>Estudante de desenvolvimento web na trybe!</h3>
-      <button type="button">Download cv</button>
-    </section>
+    <SectionMain>
+      <WelcomeText>Bem Vindo! Me chamo...</WelcomeText>
+      <PersonalName>
+        <TypeWriterEffect
+          cursorColor="white"
+          multiText={[
+            'Pedro Mesquita',
+            'Pedro Henrick',
+          ]}
+          multiTextDelay={2000}
+          typeSpeed={200}
+        />
+      </PersonalName>
+      <DescriptionText>Sou estudante de desenvolvimento web full-stack na trybe!</DescriptionText>
+      <ButtonBar>
+        <ButtonLinkedin type="button">
+          <BsLinkedin />
+          {' '}
+          LinkedIn
+        </ButtonLinkedin>
+        <ButtonDownload type="button">
+          <BiCloudDownload />
+          {' '}
+          Download cv
+        </ButtonDownload>
+      </ButtonBar>
+
+      <RedesNav>
+        <Tecnology><AiFillGithub /></Tecnology>
+        <Tecnology><FiTwitter /></Tecnology>
+      </RedesNav>
+    </SectionMain>
   );
 }
