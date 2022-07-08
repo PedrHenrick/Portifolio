8a296e0c56bad866f566d6a4cebc961aeb4a3252
Pedro Mesquita <pedro.mesquita.hc@gmail.com>
2022-07-07 21:14:40 -0300

    style: :art: Arrumando dimensões da tela inicial

--- src/components/welcome/welcomeIndex.js
+++ src/components/welcome/welcomeIndex.js
@@ -6,12 +6,12 @@ export const SectionWelcome = styled.section`
 
 export const PersonalInfo = styled.section`
   position: absolute;
-  width: 100%;
+  width: 70%;
   height: 100vh;
   background: #1C1C1C;
   display: flex;
   color: white;
   flex-direction: column;
   align-items: flex-start;
-  clip-path: polygon(0% 0%, 50% 0, 65% 100%, 0% 100%);
+  clip-path: polygon(0 0, 80% 0, 100% 100%, 0% 100%);
 `;
